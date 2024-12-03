08fb4c2a6eeaef5d7384e6be3c3d87c1f3236421
Priscila Moneo <priscila_moneo@hotmail.com.ar>
2024-12-03 17:55:34 -0300

    Cors config

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,22 @@ async function bootstrap() {
   const express = require('express');
   app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 
+  const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') || [
+    'http://localhost:3000',
+  ];
+
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     methods: 'GET, POST, PUT, DELETE',
     allowedHeaders: 'Content-Type, Authorization',
-  }, );
+  });
 
   await app.listen(3001);
-} 
+}
 bootstrap();
