33c95c45075b606fca47b08af808c5d93c27e80b
Priscila Moneo <priscila_moneo@hotmail.com.ar>
2024-12-04 10:49:44 -0300

    Added url to port

--- src/main.ts
+++ src/main.ts
@@ -9,15 +9,20 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://192.168.68.103:3000', 'https://liteflix-fe.vercel.app'],
-    methods: 'GET, POST, PUT, DELETE',
-    allowedHeaders: 'Content-Type',
+    origin: [
+      'http://localhost:3000',
+      'http://192.168.68.103:3000',
+      'https://liteflix-fe.vercel.app',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
   });
 
   await app.init();
 
-  const port = process.env.PORT || 3001;
-  server.listen(port);
+  const port = +(process.env.PORT || 3001);
+
+  server.listen(port, '0.0.0.0', () => {});
 }
 
 bootstrap();
